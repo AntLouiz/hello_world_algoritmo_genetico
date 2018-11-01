9367014bc3bda09502b0bd6358a202e7282849ad
AntLouiz <luizrodrigo46@hotmail.com>
2018-11-01 08:54:23 -0300

    removing some unuseful settings

--- settings.py
+++ settings.py
@@ -9,10 +9,6 @@ ascii_lowercase = list(string.ascii_lowercase)
 ALPHABET = ascii_lowercase + ascii_uppercase + [' ']
 
 MASTER_SOLUTION = 'hello world'
-MASTER_SOLUTION_FITNESS = 0
-
-for w in MASTER_SOLUTION:
-    MASTER_SOLUTION_FITNESS += ord(w)
 
 MAX_ITERATIONS = 200000
 INITIAL_POPULATION = 90
